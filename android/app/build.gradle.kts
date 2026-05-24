4ce44dd89bda79bbfdcf9f9e3367ad7804d31304
madderscientist <liruigang20131115@126.com>
2026-05-25 02:33:59 +0800

    fix: android network permission

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.zytx121.geochef"
     compileSdk = flutter.compileSdkVersion
-    // ndkVersion = flutter.ndkVersion
-    ndkVersion = "27.0.12077973"    // pref插件要求
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
